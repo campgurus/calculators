55888fb5327f19fff373f685604da15f4a76d7ad
Darius Goore <dariusgoore@Dariuss-MBP.fios-router.home>
2017-04-06 00:21:08 -0400

    rooting to formulas/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'formulas/index'
+  root to: 'formulas#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
