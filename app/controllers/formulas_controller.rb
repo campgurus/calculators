559ac5ab7e0618c1402247a9e75a1bab391d7b18
Darius Goore <dariusgoore@Dariuss-MBP.fios-router.home>
2017-04-07 07:41:04 -0400

    removing input from form element
    this gets rid of the auto submit behavior when user presses return.

--- app/controllers/formulas_controller.rb
+++ app/controllers/formulas_controller.rb
@@ -1,4 +1,13 @@
 class FormulasController < ApplicationController
   def index
+  	@shift_hours = []
+    
+	(18..28).each do |n|
+	  if n>23
+	    @shift_hours << n-24
+	  else
+	    @shift_hours << n 
+	  end
+	end
   end
 end
