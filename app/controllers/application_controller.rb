c4f50e4ec99e9c5fa80b785647ebeec7f574cb97
Darius Goore <dariusgoore@Dariuss-MBP.fios-router.home>
2017-04-06 23:33:05 -0400

    fixing form. cant get hours options to work yet with helper method in app_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :night_shift_hours
+
+  def night_shift_hours
+  	# create array of night shift hours
+  	night_shift_hours = []
+    
+	(18..28).each do |n|
+	  if n>23
+	    night_shift_hours << n-24
+	  else
+	    night_shift_hours << n 
+	  end
+	  return night_shift_hours
+	end  	
+  end
+
 end
